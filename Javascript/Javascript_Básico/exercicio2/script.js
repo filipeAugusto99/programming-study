9189fd458c6d03053ca48bf0a6ff0c0dfeb9edf0
Filipe Augusto <filipecordeiro213@gmail.com>
2023-01-11 18:24:13 -0300

    exercise2

--- Javascript/Javascript_Básico/exercicio2/script.js
+++ Javascript/Javascript_Básico/exercicio2/script.js
@@ -1,16 +1,17 @@
 
-let name = prompt('Digite o seu nome:');
-let inputAge = prompt('Digite a sua idade:')
+let h1 = document.querySelector('#h1')
+
+let name = prompt("Informe o seu nome:")
+let inputAge = prompt("Informe a sua idade:")
 
 let age = parseInt(inputAge)
 
-if (age >= 18 && name === 'Thomas Anderson' || age >= 18 && name === 'thomas anderson') {
-  document.write(`Olá ${name}, você é maior de idade! HOOW! Você é um personagem do filme The Matrix!!`)
+const biggerAge = 18
+  
+if (age >= 18 && name === "Thomas Anderson" || name === "thomas anderson") {
+  h1.innerHTML = `Olá ${name}, você é maior de idade e interpreta o Neo do filme The Matrix!`
+} else if(age < 18) {
+  h1.innerHTML = `Olá ${name}, você é menor de idade!`
 } else if (age >= 18) {
-  document.write(`Olá ${name}, você é maior de idade!`)
-} else {
-  document.write(`Olá ${name}, você é menor de idade!`)
+  h1.innerHTML = `Olá ${name}, você é maior de idade!`
 }
-
-
-
