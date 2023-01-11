29cc1e5d718849047ec3e0b5ff71e8ae237dace2
Filipe Augusto <filipecordeiro213@gmail.com>
2023-01-11 19:37:41 -0300

    exercise4

--- Javascript/Javascript_Básico/exercicio5/script.js
+++ Javascript/Javascript_Básico/exercicio5/script.js
@@ -1,14 +1,16 @@
+let h1 = document.querySelector('#h1')
 
-  let inputNumber = prompt("Digite um número");
-  let number = parseInt(inputNumber);
+let inputMultiplicationTable = prompt("Informe um número:")
 
-  let result = '';
+multiplicationTable = parseInt(inputMultiplicationTable)
 
-  for (let i = 1; i <= 10; i++) {
-    result = number * i;
-    document.write(number + 'x' + i + '=' + result + '<br>');
+let result = 0;
+
+if (!multiplicationTable) {
+  alert('Por favor, digite um número válido')
+} else {
+  for (let i = 0; i <= 10; i++) {
+    result = multiplicationTable * i;
+    h1.innerHTML += `${multiplicationTable} x ${i} = ${result} <br>`
   }
-
-
-
-
+}
