8054d846f61ed24e061c244a8a3437160ffda7d5
Filipe Augusto <filipecordeiro213@gmail.com>
2023-01-15 17:50:25 -0300

    exercise1

--- Javascript/Javascript_Intermediário/exercicio1/script.js
+++ Javascript/Javascript_Intermediário/exercicio1/script.js
@@ -1,20 +1,15 @@
+const increment = document.querySelector(".increment")
+const decrement = document.querySelector(".decrement")
 
-const botaoIncrement = document.querySelector('#botaoIncrement');
-const botaoDecrement = document.querySelector('#botaoDecrement');
-const counter = document.querySelector('#counterValue');
+let value = document.querySelector(".value")
+let count = 0;
 
-let contador = 0;
+increment.addEventListener("click", function() {
+  count ++
+  value.innerHTML = count
+})
 
-function somando() {
-  contador ++;
-  counter.value = contador;
-}
-
-function subtraindo() {
-  contador --;
-  counter.value = contador;
-}
-
-botaoIncrement.addEventListener('click', somando);
-
-botaoDecrement.addEventListener('click', subtraindo);
+decrement.addEventListener("click", function() {
+  count --
+  value.innerHTML = count
+})
