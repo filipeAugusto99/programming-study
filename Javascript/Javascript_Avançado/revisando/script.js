1a4bb273db8e9dc4d3da3d22a0db8630adee4ad9
Filipe Augusto <filipecordeiro213@gmail.com>
2023-01-16 18:34:13 -0300

    revisation of AJAX, callbacks and promises

---
+++ Javascript/Javascript_Avançado/revisando/script.js
@@ -0,0 +1,88 @@
+const loadButton = document.querySelector("#botaoCarregar")
+
+/**
+ *  adição do método "then" e "catch"
+ */
+
+function exibirNaTela(dados) {
+  console.log('exibindo dados', dados)
+}
+
+function exibirErro() {
+  console.log("Ops, deu erro")
+}
+
+loadButton.onclick = () => fetch("https://jsonplaceholder.typicode.com/photos", "GET").then(exibirNaTela).catch(exibirErro)
+
+/*nao recebe mais callback como parâmetro*/
+function fetch() {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest()
+    
+    xhttp.onreadystatechange = function() {
+  
+      if(this.readyState == 4 && this.status == 200) {
+        const response = JSON.parse(this.responseText)
+
+        resolve(response)
+      }
+
+      if(this.status === 404) {
+        reject()
+      }
+    }
+    
+    xhttp.open(method, url, true)
+    xhttp.send()
+  })
+}
+
+
+/********** 
+CALLBACK
+***********
+
+const loadButton = document.querySelector("#botaoCarregar")
+
+loadButton.onclick = () => carregarFotos(exibirNaTela)
+
+function exibirNaTela(dados) {
+  console.log('exibindo dados', dados)
+}
+
+function carregarFotos(callback) {
+  const xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function() {
+
+    if(this.readyState == 4 && this.status == 200) {
+      const response = JSON.parse(this.responseText)
+      if(callback) {
+        callback(response)
+      }
+    }
+  }
+  
+  xhttp.open("GET", "https://jsonplaceholder.typicode.com/photos", true)
+  xhttp.send()
+}
+
+*/
+
+
+/***********
+  AJAX
+************
+const xhttp = new XMLHttpRequest()
+
+xhttp.onreadystatechange = function() {
+
+  if(this.readyState == 4 && this.status == 200) {
+    const response = JSON.parse(this.responseText)
+    console.log(response[0])
+    console.log(response[300])
+  }
+}
+
+xhttp.open("GET", "https://jsonplaceholder.typicode.com/photos", true)
+xhttp.send()
+ */
