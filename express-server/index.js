3efdfa1fba9110c60c1ad2026c96525a524b214b
Filipe Augusto <filipecordeiro213@gmail.com>
2023-01-21 12:25:27 -0300

    serving static and public files

--- express-server/index.js
+++ express-server/index.js
@@ -2,16 +2,38 @@
 express é uma função, temos que chama-lá,
 essa função irá retornar uma aplicação em express
 */
+//MVC - MODEL VIEW CONTROLLER
 const express = require("express")
+const path = require("path")
 const app = express()
 
+//definindo o template engine
+app.set('view engine', 'ejs')
+
+
+//definindo os arquivos estaticos
+/*
+const staticFolder = path.join(__dirname, "views")
+const expressStatic = express.static(staticFolder)
+app.use(expressStatic)
+*/
+
+//definindo os arquivos publicos
+const publicFolder = path.join(__dirname, "public")
+const expressPublic = express.static(publicFolder)
+app.use(expressPublic)
+
+
+//rotas
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.render("index")
 })
 
 app.get("/about", (req, res) => {
   res.send("about")
 })
+
+
 
 //404 error (not found)
 app.use((req, res) => { //middleware
