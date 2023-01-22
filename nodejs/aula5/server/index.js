18e7c27b91d88f86aa3e40619b2b14be88b9a8fc
Filipe Augusto <filipecordeiro213@gmail.com>
2023-01-22 14:48:46 -0300

    remaking code

--- nodejs/aula5/server/index.js
+++ nodejs/aula5/server/index.js
@@ -1,18 +1,19 @@
 const http = require('http')
 const fs = require('fs')
 
+
 const server = http.createServer(function(request, response) {
-  console.log(request)
   
   if(request.url === "/") {
     fs.readFile("../client/index.html", function(error, content) {
       response.end(content)
-  
-    })}
+    })
+  }
 
   if(request.url === "/testing") {
     response.end("testando")
   }
+
 })
 
 server.listen(8080)
